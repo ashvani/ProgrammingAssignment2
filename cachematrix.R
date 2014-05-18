5260bccaf308d59919072e2c1ff018c48edcec4d
ashvani <dubey.ashvanikumar@gmail.com>
2014-05-19 00:04:44 +0530

    Complete assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+### To compute the inverse of matrix x(say) these two function can be jointly used as 
+### mySolve <- function(x, ...){
+###         List <- makeCacheMatrix(x)
+###         cacheSolve(List)
+###}
+### now call mySolve(x)
+
+# function makeCacheMatrix returns a list of four functions namely set, get, setInverse, getInverse.
+# function set caches matrix using assingment operator <<-.
+# function get returns matrix.
+# function setInverse caches inverese of matrix using operator <<-.
+# function getInverse returns inverse of matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Result <- NULL
+        set<- function(y){
+                x <<- y
+                Result <<- NULL # 'Result' is the inverse of matrix which is kept as NULL initially
+        }                       
+        get <- function() x  # function get returns input matrix
+        setInverse <- function(Inverse)
+                Result <<- Inverse  # function setInverse sets inverse of matrix in variable Result
+        getInverse <- function() Result
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+       # function makeCacheMatrix returns a list having four above functions 
 }
 
 
-## Write a short comment describing this function
+ ## function cacheSolve takes a list object x and arguments via '...' 
+ ## this function first checks that inverse of matrix is already calculated or not 
+ ## if inverse is already calculated then function getInverse returns a non-NULL value i.e.
+ ## inverse of input matrix, otherwise if getInverse returns NULL then function cacheSolve calls gets 
+ ## matrix, sets inverse of this in cache and returns inverse .
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        Inv <- x$getInverse()
+        if(!is.null(Inv)){ # checks that getInverse function returns null or not  
+                return(Inv) # if getInveres returns non-NULL
+        }
+        ## getInverse returns NULL i.e. matrix x is encountered first time
+        mat <- x$get()
+        Result <- solve(mat)
+        x$setInverse(Result)
+        Result
 }
+
